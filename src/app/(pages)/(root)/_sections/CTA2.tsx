2c91fd8e0809967c770f3722d788525540f4c1f1
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-10-14 21:59:54 +0100

    refactor: cleaned up with better css and made table responsive

--- src/app/(pages)/(root)/_sections/CTA2.tsx
+++ src/app/(pages)/(root)/_sections/CTA2.tsx
@@ -1,26 +1,30 @@
-import CardBase from "@/components/molecules/CardBase";
 import Container from "@/components/_layout/Container";
 import Section from "@/components/_layout/Section";
+import CardBase from "@/components/molecules/CardBase";
 
 function SectionCTA2() {
   return (
     <Section id="cta2" spacing="none">
       <div style={{ backgroundImage: 'url("/images/it-technician-server-maintenance_20251014.jpg")' }} className="rounded-bl-lg rounded-br-lg">
+        <Container>
 
-        <Container className="h-[900px] lg-h-auto lg:py-[190px] flex lg:block">
-          <div className="flex justify-end p-4 pb-8 lg:p-2.5 mt-auto">
-            <CardBase item={{
-              icon: "/icons/icon-5.svg",
-              title: "Integer justo quam, faucibus nec metus",
-              desc: "Ut a erat dolor. Nullam tortor diam, pretium vel massa non, volutpat condimentum nisi.",
-              button: {
-                label: "Contact our team",
-                url: "#"
-              }
-            }} />
+          <div className="flex flex-col md:block h-[900px] md:h-auto md:py-[190px]">
+            <div className="flex justify-end items-end p-4 pb-8 md:p-2.5 mt-auto">
+              <CardBase
+                item={{
+                  icon: "/icons/icon-5.svg",
+                  title: "Integer justo quam, faucibus nec metus",
+                  desc: "Ut a erat dolor. Nullam tortor diam, pretium vel massa non, volutpat condimentum nisi.",
+                  button: {
+                    label: "Contact our team",
+                    url: "#",
+                  },
+                }}
+              />
+            </div>
           </div>
-        </Container>
 
+        </Container>
       </div>
     </Section>
   );
